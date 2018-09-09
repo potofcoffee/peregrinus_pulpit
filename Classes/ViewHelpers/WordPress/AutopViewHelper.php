86daf94e5f216b1d8337edd86bcc1a35264499cd
Christoph Fischer <chris@toph.de>
2018-09-09 21:02:07 +0200

    [FIX] Sermon text appears as single long block
    This is due to a missing use of wp_autop(). Introduced new AutopViewHelper (wp:autop) and added it to sermon single view.

---
+++ Classes/ViewHelpers/WordPress/AutopViewHelper.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * PULPIT
+ * A sermon plugin for WordPress
+ *
+ * Copyright (c) 2017 Christoph Fischer, http://www.peregrinus.de
+ * Author: Christoph Fischer, chris@toph.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Peregrinus\Pulpit\ViewHelpers\WordPress;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class TranslateViewHelper
+ * Exposes WordPress' wp_autop() function to Fluid
+ * @package Peregrinus\Pulpit\ViewHelpers
+ */
+class AutopViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * @var boolean
+     */
+    protected $escapeChildren = false;
+    /**
+     * @var boolean
+     */
+    protected $escapeOutput = false;
+
+
+    /**
+     * Render the output a WP function
+     * @return string
+     */
+    public function render()
+    {
+        return wpautop($this->renderChildren());
+    }
+
+}
