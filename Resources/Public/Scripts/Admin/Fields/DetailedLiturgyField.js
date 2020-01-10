2abbed30683a08460674bc109024be5fe3f8f39a
Christoph Fischer <chris@toph.de>
2020-01-10 08:44:19 +0100

    Small adaptations for WP 5.3.2

--- Resources/Public/Scripts/Admin/Fields/DetailedLiturgyField.js
+++ Resources/Public/Scripts/Admin/Fields/DetailedLiturgyField.js
@@ -1,81 +1,84 @@
 
 var activeInput;
 
-function enableDetailedLiturgyFieldButtons() {
-    $('.pulpit-detailed-liturgy-form-single-toggle').click(function(){
-        $(this).parent().find('.pulpit-detailed-liturgy-form-sub').toggle();
-        if ($(this).hasClass('ui-icon-arrowthick-1-s')) {
-            $(this).removeClass('ui-icon-arrowthick-1-s').addClass('ui-icon-arrowthick-1-n');
-        } else {
-            $(this).removeClass('ui-icon-arrowthick-1-n').addClass('ui-icon-arrowthick-1-s');
-        }
-    });
 
-    $('.pulpit-detailed-liturgy-field-btn-remove').click(function(event){
-        event.preventDefault();
-        $(this).parent().remove();
-    });
-
-    $('.pulpit-song-selectbox').select2({
-        allowClear: true,
-        placeholder: '',
-        tags: true
-    });
-    $('.pulpit-song-selectbox')
-        .on('select2:select', function(e){
-            $('#'+$(this).parent().data('preview')+' .data-preview-song').html(e.params.data.text);
-        });
+(function($){
 
 
-    $('.pulpit-detailed-liturgy-form-single textarea').on('focus', function(){
-        activeInput = this;
-    });
-
-
-    $('.pulpit-detailed-liturgy-field-btn-paste-template').click(function(event){
-        event.preventDefault();
-
-        $.post({
-            url: ajaxurl,
-            data: {
-                'action': 'pulpit_renderTextBlock',
-                'id': $(this).parent().find('select').first().val(),
-            },
-            context: this,
-            success: function(response) {
-                $(activeInput).replaceSelectedText(response);
-                $(activeInput).trigger('change');
+    function enableDetailedLiturgyFieldButtons() {
+        $('.pulpit-detailed-liturgy-form-single-toggle').click(function(){
+            $(this).parent().find('.pulpit-detailed-liturgy-form-sub').toggle();
+            if ($(this).hasClass('ui-icon-arrowthick-1-s')) {
+                $(this).removeClass('ui-icon-arrowthick-1-s').addClass('ui-icon-arrowthick-1-n');
+            } else {
+                $(this).removeClass('ui-icon-arrowthick-1-n').addClass('ui-icon-arrowthick-1-s');
             }
         });
 
+        $('.pulpit-detailed-liturgy-field-btn-remove').click(function(event){
+            event.preventDefault();
+            $(this).parent().remove();
+        });
 
-
-    });
-
-    $('.pulpit-detailed-liturgy-extended-select').select2({});
-
-}
-
-
-function createSortableList() {
-    $('.pulpit-detailed-liturgy-form').sortable({
-        placeholder: "ui-state-highlight",
-        update: function(event, ui) {
-            // reorder indices
-            $(this).find('.pulpit-detailed-liturgy-form-single').each(function(index, linkElement){
-                $(linkElement).find('input,select,textarea').each(function(subIndex, inputElement){
-                    $(inputElement).attr('name', $(this).attr('name').replace(/(\d+)/g, index));
-                    $(inputElement).attr('id', $(this).attr('id').replace(/(\d+)/g, index));
-                });
-                $(linkElement).find('label').each(function(subIndex, labelElement) {
-                    $(labelElement).attr('for', $(this).attr('for').replace(/(\d+)/g, index));
-                });
+        $('.pulpit-song-selectbox').select2({
+            allowClear: true,
+            placeholder: '',
+            tags: true
+        });
+        $('.pulpit-song-selectbox')
+            .on('select2:select', function(e){
+                $('#'+$(this).parent().data('preview')+' .data-preview-song').html(e.params.data.text);
             });
-        }
-    }).disableSelection();
-}
 
-(function($){
+
+        $('.pulpit-detailed-liturgy-form-single textarea').on('focus', function(){
+            activeInput = this;
+        });
+
+
+        $('.pulpit-detailed-liturgy-field-btn-paste-template').click(function(event){
+            event.preventDefault();
+
+            $.post({
+                url: ajaxurl,
+                data: {
+                    'action': 'pulpit_renderTextBlock',
+                    'id': $(this).parent().find('select').first().val(),
+                },
+                context: this,
+                success: function(response) {
+                    $(activeInput).replaceSelectedText(response);
+                    $(activeInput).trigger('change');
+                }
+            });
+
+
+
+        });
+
+        $('.pulpit-detailed-liturgy-extended-select').select2({});
+
+    }
+
+
+    function createSortableList() {
+        $('.pulpit-detailed-liturgy-form').sortable({
+            placeholder: "ui-state-highlight",
+            update: function(event, ui) {
+                // reorder indices
+                $(this).find('.pulpit-detailed-liturgy-form-single').each(function(index, linkElement){
+                    $(linkElement).find('input,select,textarea').each(function(subIndex, inputElement){
+                        $(inputElement).attr('name', $(this).attr('name').replace(/(\d+)/g, index));
+                        $(inputElement).attr('id', $(this).attr('id').replace(/(\d+)/g, index));
+                    });
+                    $(linkElement).find('label').each(function(subIndex, labelElement) {
+                        $(labelElement).attr('for', $(this).attr('for').replace(/(\d+)/g, index));
+                    });
+                });
+            }
+        }).disableSelection();
+    }
+
 
     $(document).ready(function(){
 
